814971d789b2e277a31e3e28c46be0ad5a0f2644
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-04-07 15:51:52 +0200

    Add doc to BaseWorker.find_model

--- stageit/BaseWorker.py
+++ stageit/BaseWorker.py
@@ -4,6 +4,9 @@ from stageit.BaseDevice import BaseDevice
 
 
 class BaseWorker(threading.Thread):
+    """
+    Base class to connect to network device
+    """
 
     def __init__(self, q, hostname, port, transport):
         self.q = q
@@ -16,15 +19,22 @@ class BaseWorker(threading.Thread):
         while True:
             try:
                 self.work = self.q.get(timeout=60)
-                self.status = "Discovering model"
-                driver = self.find_model(**self.work)
 
             except queue.Empty():
                 return
 
+        self.status = "Discovering model"
+        driver = self.find_model(**self.work)
+
+        
         self.q.task_done()
 
     def find_model(self, **kwargs):
+        """
+        Find device type and return appropriate class to deal with upgrading,
+        version checking and else
+        """
+
         device = BaseDevice(**kwargs)
         self.status = "Connecting to " + device.facts["model"]
         if "C3650" in device.facts["model"]:
