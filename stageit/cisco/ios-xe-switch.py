26c97e35bc5d2612ff0f0d8b104c7e9966b2ce92
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-04-07 10:57:48 +0200

    add IOSXESwitch

--- stageit/cisco/ios-xe-switch.py
+++ stageit/cisco/ios-xe-switch.py
@@ -1,6 +1,18 @@
 from stageit.BaseDevice import BaseDevice
 
+
 class IOSXESwitch(BaseDevice):
+    def firmware_ok(self, firmware):
+        with self.driver(**self.sessiondata) as session:
+            showver = session.device.send_command("show ver")
+
+            # This regex parses the following output:
+
+            verregex = r"^\*? *(\d) (\d{1,2}) *([A-Z\-0-9]*) *([A-Z\-0-9.]*) *([A-Za-z\-_0-9.]*) ([A-Z])*"
+            switches = re.self
+
+
     def upgrade_switch(self):
-        # regex to find switch stack ^\*? *(\d) (\d{1,2}) *([A-Z\-0-9])* *([A-Z\-0-9.])* *([A-Za-z\-_0-9.])* ([A-Z])*
-        pass
+        import re
+        # regex to find switch stack 
+        
