571422be3af78e91524d5e35144bdadea7099a8a
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-04-07 10:57:36 +0200

    Add base worker for threads

---
+++ stageit/BaseWorker.py
@@ -0,0 +1,35 @@
+import threading
+import queue
+from stageit import BaseDevice
+
+class BaseWorker(threading.Thread):
+    def __init__(self, q, hostname, port, transport):
+        self.q = q
+        self.hostname = hostname
+        self.port = port
+        self.transport = transport
+        self.status = "Initializing"
+
+    def run(self):
+        while True:
+            try:
+                self.work = self.q.get(timeout=60)
+                self.status = "Discovering model"
+                driver = find_model(**self.work)
+
+            except queue.Empty():
+                return
+        
+        self.q.task_done()
+
+    def find_model(self, **kwargs):
+        device = BaseDevice(**kwargs)
+        self.status = "Connecting to " + device.facts["model"]
+        if "C3650" in device.facts["model"]:
+            from stageit.cisco.ios-xe-switch import IOSXESwitch
+            specific_device = IOSXESwitch(**kwargs)
+        
+        else:
+            raise ValueError("Unrecognised model")
+        
+        return specific_device
