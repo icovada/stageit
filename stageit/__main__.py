f8dcddfd28c102e7040fc53195b5f88d664b5866
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-04-19 12:01:49 +0200

    Move Flask to own module

--- stageit/__main__.py
+++ stageit/__main__.py
@@ -1,56 +1,22 @@
 import yaml
 import json
 import queue
-from flask import Flask, request, stream_with_context, Response
 from time import sleep
 import sys
 import os
-
-sys.path.append( os.path.join( os.path.dirname(__file__), os.path.pardir ) ) 
-
+import sqlalchemy
 from libs.FakeWorker import FakeWorker
 from libs.BaseWorker import BaseWorker
-app = Flask(__name__)
+import flaskapp
+import config
 
-
-worker_array = {}
-
-@app.route("/log/<worker>")
-def log(worker):
-    def streambytes():
-        oldposition = 0
-        while worker_array[worker]['thread'].status != "Waiting for work":
-            sleep(0.1)
-            newbuffer = worker_array[worker]['thread'].driver.getlog()
-            yield newbuffer[oldposition:].replace("\n","<br/>")
-            oldbuffer = newbuffer
-            oldposition = len(oldbuffer)
-
-    try:
-        if worker_array[worker]['thread'].status != "Waiting for work":
-            return Response(stream_with_context(streambytes()))
-        else:
-            return "No work queued, come back later"
-    except AttributeError:
-        return "Discovering platform, please refresh later"
-
-@app.route("/jobstatus/<worker>")
-def jobstatus(worker):
-    return worker_array[worker]['thread'].getstatus()
-
-@app.route('/enqueue/<worker>', methods = ['POST'])
-def enqueue(worker):
-    queueme = request.form.copy()
-    queueme['tempconfig'] = json.loads(request.form['tempconfig'])
-    queueme['finalconfig'] = json.loads(request.form['finalconfig'])
-
-    worker_array[worker]['queue'].put(queueme)
-    return "OK"
+sys.path.append( os.path.join( os.path.dirname(__file__), os.path.pardir ) ) 
 
 if __name__ == '__main__':
     with open('stageit/term_config.yaml', 'r') as y:
         config = yaml.load(y)
 
+    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
 
     for entry in config['terminal_server']:
         server_name = "{}:{}".format(entry['hostname'], entry['port'])
@@ -60,7 +26,7 @@ if __name__ == '__main__':
         else:
             server_thread = BaseWorker(thisqueue, **entry)
         server_thread.start()
-        worker_array[server_name] = {
+        config.worker_array[server_name] = {
             'queue': thisqueue, 'thread': server_thread}
 
-    app.run(threaded=True)
+    flaskapp.run()
---
+++ stageit/flaskapp.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, stream_with_context, Response
+import config
+from time import sleep
+import json
+
+@app.route("/log/<worker>")
+def log(worker):
+    def streambytes():
+        oldposition = 0
+        while config.worker_array[worker]['thread'].status != "Waiting for work":
+            sleep(0.1)
+            newbuffer = config.worker_array[worker]['thread'].driver.getlog()
+            yield newbuffer[oldposition:].replace("\n","<br/>")
+            oldbuffer = newbuffer
+            oldposition = len(oldbuffer)
+
+    try:
+        if config.worker_array[worker]['thread'].status != "Waiting for work":
+            return Response(stream_with_context(streambytes()))
+        else:
+            return "No work queued, come back later"
+    except AttributeError:
+        return "Discovering platform, please refresh later"
+
+@app.route("/jobstatus/<worker>")
+def jobstatus(worker):
+    return config.worker_array[worker]['thread'].getstatus()
+
+@app.route('/enqueue/<worker>', methods = ['POST'])
+def enqueue(worker):
+    queueme = request.form.copy()
+    queueme['tempconfig'] = json.loads(request.form['tempconfig'])
+    queueme['finalconfig'] = json.loads(request.form['finalconfig'])
+
+    config.worker_array[worker]['queue'].put(queueme)
+    return "OK"
+
+app = Flask(__name__)
