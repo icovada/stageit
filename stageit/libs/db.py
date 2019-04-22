cd353522c9c8dd62e77009f264767dd64239a63c
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-04-22 12:41:48 +0200

    move db.py inside libs

--- stageit/libs/db.py
+++ stageit/libs/db.py
@@ -31,6 +31,6 @@ class History(Base):
 # sqlalchemy_example.db file.
 engine = create_engine('sqlite:///sqlalchemy_example.db')
 
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
-Base.metadata.create_all(engine)
+
+if '__name__' = '__main__':
+    Base.metadata.create_all(engine)
