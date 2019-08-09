b6144dd97ee96f504215c44bf0886c8e2f91c5dd
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-08-09 12:35:12 +0200

    Add database schema

--- stageitweb/stageitweb/stageit/models.py
+++ stageitweb/stageitweb/stageit/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Templates(models.Model):
     """Defines templates table."""
+    pkid = models.UUIDField(primary_key=True, )
     description = models.TextField(max_length=50)
     filepath = models.TextField(max_length=256)
     installmode = models.TextField(max_length=20)
@@ -11,4 +12,26 @@ class Templates(models.Model):
     poststaging = models.TextField
     template = models.TextField
     templatevalues = models.BinaryField
-    tasks = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+
+class History(models.Model):
+    """Defines history table."""
+    pkid = models.UUIDField(primary_key=True)
+    dateend = models.DateTimeField
+    datestart = models.DateTimeField
+    description = models.TextField(max_length=50)
+    installmode = models.TextField(max_length=20)
+    model = models.TextField(max_length=50)
+    os_version = models.TextField(max_length=300)
+    rundata = models.BinaryField(max_length=1024000)
+    serial = models.TextField(max_length=20)
+    serial_number = models.TextField(max_length=50)
+    template = models.TextField(max_length=20000)
+    templatevalues = models.BinaryField(max_length=4096)
+    vendor = models.TextField(max_length=30)
+
+class Tasks(models.Model):
+    """Defines tasks table."""
+    pkid = models.UUIDField(primary_key=True)
+    description = models.TextField(max_length=50)
+    fktemplate = models.ForeignKey(Templates, on_delete=models.CASCADE)
+    taskvalues = models.BinaryField(max_length=4096)
