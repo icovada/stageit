ef2c05a47afa4379e78ce04eaea1a614baa83354
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-08-09 11:29:28 +0200

    MIgrate templates to Django path

--- stageitweb/stageitweb/stageit/apps.py
+++ stageitweb/stageitweb/stageit/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class StageitConfig(AppConfig):
-    name = 'stageit'
+    name = 'stageitweb.stageit'
--- stageitweb/stageitweb/stageit/views.py
+++ stageitweb/stageitweb/stageit/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("StageIT!")
+    return render(request, 'stageit/home.html')
