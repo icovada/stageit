9a5c71aaa286834112eeb26165228ec5cdbc03e4
Federico Tabbò (Europe) <federico.tabbo@dimensiondata.com>
2019-08-09 11:46:27 +0200

    Add base page views

--- stageitweb/stageitweb/stageit/urls.py
+++ stageitweb/stageitweb/stageit/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('templates', views.templates, name='templates'),
+    path('history', views.history, name='history'),
+    path('tasks', views.tasks, name='tasks'),
     path('', views.index, name='index'),
 ]
--- stageitweb/stageitweb/stageit/views.py
+++ stageitweb/stageitweb/stageit/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'stageit/home.html')
+
+def templates(request):
+    return render(request, 'stageit/templates.html')
+
+def history(request):
+    return render(request, 'stageit/history.html')
+
+def tasks(request):
+    return render(request, 'stageit/tasks.html')
