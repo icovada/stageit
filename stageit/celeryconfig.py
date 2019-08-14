0c9987284fe2d76f5ad434d6623cd71eae86585e
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2019-08-14 20:42:13 +0200

    Remove result serializer (migrated to callbacks

--- stageit/celeryconfig.py
+++ stageit/celeryconfig.py
@@ -1,11 +1,9 @@
 broker_url = 'amqp://'
-result_backend = 'amqp://'
 
-task_serializer = 'pickle'
-result_serializer = 'pickle'
-accept_content = ['pickle']
+task_serializer = 'json'
+accept_content = ['json']
 timezone = 'Europe/Rome'
 enable_utc = True
 
-imports = ("stageit.libs.fake_worker", 
-           "stageit.libs.base_worker", )
+imports = ("stageit.libs.fake_worker", )
+           #"stageit.libs.base_worker", )
