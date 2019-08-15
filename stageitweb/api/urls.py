ce617a176e0b286ce3cc1b3de7cf8f38a9e67c2e
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2019-08-16 01:43:38 +0200

    All API names are singular

--- stageitweb/api/urls.py
+++ stageitweb/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from stageitweb.api import views
 
 router = routers.DefaultRouter()
-router.register(r'templates', views.TemplatesViewSet)
-router.register(r'tasks', views.TasksViewSet)
+router.register(r'template', views.TemplatesViewSet)
+router.register(r'task', views.TasksViewSet)
 router.register(r'history', views.HistoryViewSet)
 router.register(r'log', views.LogViewSet)
 router.register(r'terminalserver', views.TerminalServerViewSet)
