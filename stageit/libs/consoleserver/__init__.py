c3faa5870813aa9bf3cf49fa92c930a493e7d2e2
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2019-08-16 01:31:59 +0200

    Pass arguments to terminal server as kwargs

--- stageit/libs/consoleserver/__init__.py
+++ stageit/libs/consoleserver/__init__.py
@@ -6,12 +6,12 @@ To be expanded by platform-specific modules
 
 class BaseConsoleServer():
     """Base class"""
-    def __init__(self, hostname, username, password, transport, line, **kwargs):
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-        self.transport = transport
-        self.line = line
+    def __init__(self, **kwargs):
+        self.hostname = kwargs.get('hostname')
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
+        self.transport = kwargs.get('transport')
+        self.line = kwargs.get('line')
 
     def reset(self):
         """Expand me"""
