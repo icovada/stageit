3768a649f57899a2672c243347302cc59213f106
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2019-11-11 18:16:01 +0100

    Revert 55b7fc8

--- stageit/libs/cisco/switch/iosxe.py
+++ stageit/libs/cisco/switch/iosxe.py
@@ -11,12 +11,12 @@ class IOSXESwitch(BaseDevice):
         """Verify software version and if necessary upgrade through proper path."""
         logging.info("Checking firmware version")
         firmware = (False, None, None)
-        if re.search(r'cat3k_caa-universalk9(ldpe)?(\.(\d{2})){3}\.SPA\.bin', uri):
+        if re.search(r'cat3k_caa-universalk9(?:ldpe)*\.(\d*\w*\.\d*\w*\.\d*\w*)\.', uri):
             # Catalyst 3650, 3750
             # cat3k_caa-universalk9.16.06.06.SPA.bin
             # cat3k_caa-universalk9ldpe.16.06.06.SPA.bin
             version = re.findall(
-                r'cat3k_caa-universalk9(ldpe)?(\.(\d{2})){3}\.SPA\.bin', uri)[0]
+                r'cat3k_caa-universalk9(?:ldpe)*\.(\d*\w*\.\d*\w*\.\d*\w*)\.', uri)[0]
 
         else:
             self.session.close()
