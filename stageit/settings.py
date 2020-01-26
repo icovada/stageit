8539410c8635fbec763051537d8cafc99b16c3fa
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-01-26 15:05:33 +0000

    Move core files to root folder

--- stageit/settings.py
+++ stageit/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stageitweb.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stageitweb.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
