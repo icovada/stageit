38780619b676fd04774839019acdc0427644faa2
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-01-31 14:57:09 +0100

    Move django settings to env file

--- stageit/settings.py
+++ stageit/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+&-^6o7d_g^$p4&koa7i9j2ln(ok5w-0q0ng0znolk(7m(u6ny'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
