da07f3cd05a242394816f022fce0d268a8aa7495
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-02-20 16:52:46 +0000

    Initial scheduler with fake device

--- stageit-worker/scheduler.py
+++ stageit-worker/scheduler.py
@@ -1,9 +1,31 @@
 import time
 import schedule
-import requests
+import os
+from requests import get
+from libs.base_device import base_device
+from libs.fake_device import fake_device
+
+worker_id = os.environ['WORKER_ID']
+stageit_endpoint = os.environ['STAGEIT_ENDPOINT']
+token = os.environ['TOKEN']
+history_url = stageit_endpoint + "/api/history/?format=json&search=Queued"
 
 def check_tasks():
-    pass
+    try:
+        history_list = get(history_url)
+    except:
+        pass
+
+    if len(history_list) == 0:
+        return True
+
+    for history in history_list:
+        if history['status'] == 'Queued' and history['fkremoteworker'] == worker_id:
+            fake_device()
+
+
+
+my_pkid = get(stageit_endpoint + "/?name=" + worker_id)
 
 schedule.every(10).seconds.do(check_tasks)
 
