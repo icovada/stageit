79afb97ed9340bed635782897ac7b265244fdb31
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-06-02 17:38:56 +0200

    Serial Port description in popup title

--- stageit-web/web_interface/signals.py
+++ stageit-web/web_interface/signals.py
@@ -1,11 +1,10 @@
-from django.contrib.auth.models import User
+from datetime import datetime as dt
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_eventstream import send_event
 
 from web_interface.models import History
 
-from datetime import datetime as dt
 
 @receiver(post_save, sender=History)
 def history_popup(sender, instance, created, **kwargs):
@@ -14,7 +13,7 @@ def history_popup(sender, instance, created, **kwargs):
     minute = now.minute
     second = now.second
 
-    title = instance.status
-    link = f'<a href="/history/{instance.pkid}">Open</a>'
-    footer = f'{hour}:{minute}:{second}'
+    title = instance.fkserialport.description
+    link = f'<a href="/history/{instance.pkid}">{instance.status}</a>'
+    footer = f'{hour:02}:{minute:02}:{second:02}'
     send_event('notifications', 'notification', {'title': title, 'link': link, 'footer': footer})
