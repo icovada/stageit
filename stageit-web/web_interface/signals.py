76ee9ccaba3ae8b9d0f5be947f4bdf2f8518e8b4
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-06-02 20:35:10 +0200

    Popup on every History update

--- stageit-web/web_interface/signals.py
+++ stageit-web/web_interface/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_eventstream import send_event
 
-from web_interface.models import History
+from web_interface.models import History, SerialPort
 
 
 @receiver(post_save, sender=History)
@@ -13,7 +13,8 @@ def history_popup(sender, instance, created, **kwargs):
     minute = now.minute
     second = now.second
 
-    title = instance.fkserialport.description
+    serialport = SerialPort.objects.get(pkid=instance.fkserialport)
+    title = serialport.description
     link = f'<a href="/history/{instance.pkid}">{instance.status}</a>'
     footer = f'{hour:02}:{minute:02}:{second:02}'
     send_event('notifications', 'notification', {'title': title, 'link': link, 'footer': footer})
