f70d94e11d288a8798f89596aa96b013942404d8
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-06-02 19:20:19 +0200

    BREAKING change, rundata is jsonfield not binary

--- stageit-web/web_interface/models.py
+++ stageit-web/web_interface/models.py
@@ -57,7 +57,7 @@ class History(models.Model):
     installmode = models.TextField(max_length=20)
     model = models.TextField(max_length=50)
     os_version = models.TextField(max_length=300)
-    rundata = models.BinaryField(max_length=1024000, editable=True, null=True)
+    rundata = JSONField(null=True)
     serial = models.TextField(max_length=20)
     template = models.TextField(max_length=20000)
     templatevalues = JSONField(null=True)
@@ -118,7 +118,7 @@ class SerialPort(models.Model):
         unique_together = ('fkterminalserver', 'port',)
 
     def __str__(self):
-        return('{} - {}'.format(self.fkterminalserver, self.line))
+        return('{} - {} - {}'.format(self.fkterminalserver, self.line, self.description))
     
     def get_absolute_url(self):
         return(str(self.pkid))
