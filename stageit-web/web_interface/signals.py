72c29ebee4690520e854c9225d7c9743f0022403
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-06-02 20:56:26 +0200

    Send signal to refresh history detail page

--- stageit-web/web_interface/signals.py
+++ stageit-web/web_interface/signals.py
@@ -5,9 +5,13 @@ from django_eventstream import send_event
 
 from web_interface.models import History, SerialPort
 
-
 @receiver(post_save, sender=History)
 def history_popup(sender, instance, created, **kwargs):
+    """
+    Send a message through server events every time a History line is updated
+
+    TODO: Only send the popup when the status acutally changes
+    """
     now = dt.now()
     hour = now.hour
     minute = now.minute
@@ -18,3 +22,28 @@ def history_popup(sender, instance, created, **kwargs):
     link = f'<a href="/history/{instance.pkid}">{instance.status}</a>'
     footer = f'{hour:02}:{minute:02}:{second:02}'
     send_event('notifications', 'notification', {'title': title, 'link': link, 'footer': footer})
+
+
+@receiver(post_save, sender=History)
+def history_table_refresh(sender, instance, created, **kwargs):
+    """
+    Send data of History page for live refresh
+
+    """
+
+    pkid = instance.pkid
+    topic = "history-" + str(pkid)
+    
+    data = {'status': instance.status,
+            'datestart': instance.datestart,
+            'dateend': instance.dateend,
+            'description': instance.description,
+            'vendor': instance.vendor,
+            'model': instance.model,
+            'serial': instance.serial,
+            'install_mode': instance.installmode,
+            'os_version': instance.os_version,
+            }
+
+    send_event(topic, 'table-refresh', data)
+
